f45f6d237ddbaa93cec06eb9d96f3ab93aa41a36
mizisu <alwltn960406@gmail.com>
2019-08-13 16:07:47 +0900

    edit testcase and edit README.md

--- DotGrok.Test/GrokTest.cs
+++ DotGrok.Test/GrokTest.cs
@@ -4,6 +4,7 @@ namespace DotGrok.Test
     using System.Collections.Generic;
     using System.Diagnostics;
     using System.Text;
+    using System.Linq;
     using Xunit;
     using Xunit.Abstractions;
 
@@ -18,30 +19,30 @@ namespace DotGrok.Test
         [Fact]
         public void Test()
         {
-            var grok = DotGrok.Grok.NewBuilder(@"%{DateTime:Time} %{LogLevel:level} %{Message:message}")
+            var grok = DotGrok.Grok.NewBuilder(@"%{DateTime:time} %{LogLevel:level} %{Message:message}")
                 .AddPattern("DateTime", @"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3}")
-                .AddPattern("LogLevel", @".{5}")
+                .AddPattern("LogLevel", @".\w+")
                 .AddPattern("Message", @".+")
-                .AddConverter("DateTime", s =>
-                {
-                    output.WriteLine(s);
-                    return DateTime.Parse(s);
-                })
+                .AddConverter("DateTime", s => DateTime.Parse(s))
                 .Build();
 
-            var lines = new string[]{
-                "2018-01-01 12:32:23.345 INFO test message 1233tdsg"
-                };
+            var r = grok.Match("2018-01-01 12:32:23.345 INFO test message 1233tdsg");
 
-            foreach (var item in lines)
+            var items = r.Items.ToList();
+
+            foreach (var item in r.Items)
             {
-                var r = grok.Match(item);
-                output.WriteLine($"Result : {r.Success}");
-                foreach (var resultItem in r.Items)
-                {
-                    output.WriteLine($"{resultItem.Name} : {resultItem.Value}");
-                }
+                output.WriteLine($"{item.Name} : {item.Value}");
             }
+
+            Assert.Equal(items[0].Name, "time");
+            Assert.Equal(items[0].Value, new DateTime(2018, 01, 01, 12, 32, 23, 345));
+
+            Assert.Equal(items[1].Name, "level");
+            Assert.Equal(items[1].Value, "INFO");
+
+            Assert.Equal(items[2].Name, "message");
+            Assert.Equal(items[2].Value, "test message 1233tdsg");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,26 +1,29 @@
 # DotGrok
+
 .NET Parse text with pattern.
 
 ## Usage
+
 ```csharp
-var grok = DotGrok.Grok.NewBuilder(@"%{DateTime:Time}")
-    .AddPattern("DateTime", @"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3}")
-    .AddPattern("LogLevel", @".{5}")
-    .AddPattern("Message", @".")
-    .AddConverter("DateTime", s => DateTime.Parse(s))
-    .Build();
+var grok = DotGrok.Grok.NewBuilder(@"%{DateTime:time} %{LogLevel:level} %{Message:message}")
+                .AddPattern("DateTime", @"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3}")
+                .AddPattern("LogLevel", @".\w+")
+                .AddPattern("Message", @".+")
+                .AddConverter("DateTime", s => DateTime.Parse(s))
+                .Build();
 
-var lines = new string[]{
-    "2018-01-01 12:32:23.345"
-    };
+var r = grok.Match("2018-01-01 12:32:23.345 INFO test message 1233tdsg");
 
-foreach (var item in lines)
+foreach (var item in r.Items)
 {
-    var r = grok.Match(item);
-    Console.WriteLine($"{r.Success}");
-    foreach (var resultItem in r.Items)
-    {
-        Console.WriteLine($"{resultItem.Name} : {resultItem.Value}");
-    }
+    System.Console.WriteLine($"{item.Name} : {item.Value}");
 }
+```
+
+## Result
+
+```
+time : 01/01/2018 12:32:23
+level : INFO
+message : test message 1233tdsg
 ```
